34f988d33d639aa7b0fd10afd779a69395c8d52c
thoxy <thi.hur67@gmail.com>
2024-02-03 22:03:52 +0100

    some progress

--- src/main.zig
+++ src/main.zig
@@ -39,16 +39,18 @@ pub fn main() !void {
 
 // TODO : Need to fix
 fn write_import_table(baseptr: ?*anyopaque, nt_header: *win.IMAGE_NT_HEADERS) void {
-    const import_dir = nt_header.OptionalHeader.DataDirectory[win.IMAGE_DIRECTORY_ENTRY_IMPORT];
+    const import_dir = nt_header.OptionalHeader.DataDirectory[@as(usize, win.IMAGE_DIRECTORY_ENTRY_IMPORT)];
     if (import_dir.Size == 0) {
         return;
     }
 
+    std.debug.print("\n{any}\n", .{import_dir.VirtualAddress});
+
     const ogfirstthunkptr: *win.IMAGE_IMPORT_DESCRIPTOR = @ptrFromInt(@intFromPtr(baseptr) + @as(usize, @intCast(import_dir.VirtualAddress)));
     while (ogfirstthunkptr.Name != 0 and ogfirstthunkptr.FirstThunk != 0) {
-        const ptr: usize = @intFromPtr(baseptr) + (ogfirstthunkptr.Name - 6);
+        const ptr: usize = @intFromPtr(baseptr) + (ogfirstthunkptr.Name) - 40;
 
-        std.debug.print("{}", .{ogfirstthunkptr.Name});
+        std.debug.print("{}\n", .{ogfirstthunkptr.Name});
 
         const dllname = read_string_from_memory(@ptrCast(@as([*]u8, @ptrFromInt(ptr))));
         std.debug.print("{s} \n", .{dllname});
@@ -57,8 +59,8 @@ fn write_import_table(baseptr: ?*anyopaque, nt_header: *win.IMAGE_NT_HEADERS) vo
 }
 
 fn read_string_from_memory(baseptr: [*]u8) []u8 {
-    var temp: [win.MAX_PATH]u8 = undefined;
-    for (0..win.MAX_PATH) |i| {
+    var temp: [100]u8 = undefined;
+    for (0..100) |i| {
         temp[i] = baseptr[i];
         if (temp[i] == 0) {
             break;
