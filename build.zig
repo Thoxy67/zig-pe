a109ad07190b8990038507ac29976911f731a123
thoxy <thi.hur67@gmail.com>
2024-02-04 15:09:50 +0100

    add build tag to reduce the size of the compiled bin

--- build.zig
+++ build.zig
@@ -21,9 +21,12 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .link_libc = true,
+        .single_threaded = true,
+        .strip = true,
+        .pic = true,
     });
     exe.linkLibC();
-
+    exe.want_lto = true;
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
